8bb310e106f557452ba1dc98981fec3c3c3d6acc
Andy Awidarto <andy.awidarto@gmail.com>
2014-01-15 14:53:19 +0700

    progress commit

---
+++ app/events.php
@@ -0,0 +1,33 @@
+<?php
+
+
+Event::listen('log.a',function($class, $method, $actor, $result){
+    $data = array(
+            'timestamp'=>new MongoDate(),
+            'class'=>$class,
+            'method'=>$method,
+            'actor'=>$actor,
+            'result'=>$result
+        );
+
+    Activelog::insert($data);
+
+    return true;
+});
+
+Event::listen('cleanup',function(){
+
+    $last = new MongoDate( time() - Config::get('ia.reserveTimeOut') );
+
+    $props = Property::where('locked','=',1)->where('reservedAt','<', $last )->get();
+
+    foreach($props as $property){
+            $property->propertyStatus = $property->propertyLastStatus;
+            $property->reservedBy = '';
+            $property->reservedAt = '';
+            $property->lock = 0;
+            $property->save();
+    }
+
+    return true;
+});
